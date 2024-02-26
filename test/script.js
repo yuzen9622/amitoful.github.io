74a64edb8f8afd4394e3a72ad0a2dad7e392b887
oscar <oscar48079@gmail.com>
2024-02-26 22:18:05 +0800

    v2.0

--- test/script.js
+++ test/script.js
@@ -1,23 +1,28 @@
 var bags = document.querySelectorAll(".bag input[name='bag']");
 var mls = document.querySelectorAll(".bag input[name='ml']");
-var totals = document.querySelectorAll(".bag input[name='total']");
+var totals = document.getElementById('total');
 var all = document.getElementById("all");
 var del = document.getElementById("del");
-var resule = document.querySelectorAll(".bag input[name='realuse']")
+var resule = document.getElementById('realuse')
 
 function calculate() {
+    var bagv=0;
+    var mlv=0;
     for (var i = 0; i < bags.length; i++) {
-        var bagv = bags[i].value;
-        var mlv = mls[i].value;
+         bagv += Number(bags[i].value);
+         mlv += Number(mls[i].value);
+         console.log(bagv,mlv)
+    }
         var totalValue = Number((parseFloat(bagv * mlv) / 1000).toFixed(2));
         var allv = all.value;
         var delv = del.value;
         var nov = allv - totalValue - delv;
         var usev = parseFloat((nov / totalValue) * 100).toFixed(1);
-        totals[i].value = totalValue;
-        resule[i].value = usev;
+        console.log(totalValue)
+        totals.value = totalValue;
+        resule.value = usev;
 
-    }
+    
 
 }
 for (var i = 0; i < bags.length; i++) {
